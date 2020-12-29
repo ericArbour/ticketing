d77bd71c493af4dc28a97bfe5d92b9eb92a5f7f7
Eric Arbour <arboureric@gmail.com>
2020-12-29 15:42:30 -0500

    Move errors and middlewares to common

---
+++ common/src/middlewares/error-handler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { CustomError } from '../errors/custom-error';
+
+/*
+  If an error is thrown by any previous express handler, this
+  middelware will run and allow us to standardize our error
+  response.
+*/
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  res.status(400).send({ errors: [{ message: 'Something went wrong' }] });
+};
