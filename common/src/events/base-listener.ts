bd978d74fad37d27cb55b8fd620e3764eef98577
Eric Arbour <arboureric@gmail.com>
2021-01-24 13:23:41 -0500

    Make listener/publisher client field protected so subclass can access it

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -10,7 +10,7 @@ export abstract class Listener<T extends Event> {
   abstract subject: T['subject'];
   abstract queueGroupName: string;
   abstract onMessage(data: T['data'], msg: Message): void;
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
@@ -30,7 +30,7 @@ export abstract class Listener<T extends Event> {
     const subscription = this.client.subscribe(
       this.subject,
       this.queueGroupName,
-      this.subscriptionOptions()
+      this.subscriptionOptions(),
     );
 
     subscription.on('message', (msg: Message) => {
