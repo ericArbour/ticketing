ac61568b24e975d980f4d041828765f4822db8c0
Eric Arbour <arboureric@gmail.com>
2021-01-30 13:28:26 -0500

    Finish section 20 - expiration service

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@earbtickets/common';
+
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject = Subjects.OrderCreated as const;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      },
+    );
+
+    msg.ack();
+  }
+}
